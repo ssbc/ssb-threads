cc9b58e8701307595a734330b1b2d38a74e3a2c2
Andre Staltz <andre@staltz.com>
2018-05-25 12:27:10 +0300

    release 2.0.0

--- index.ts
+++ index.ts
@@ -230,7 +230,7 @@ function init(ssb: any, config: any) {
 
 export = {
   name: 'threads',
-  version: '1.1.0',
+  version: '2.0.0',
   manifest: {
     public: 'source',
     publicUpdates: 'source',
