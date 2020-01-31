d7835fecc60785a685d00976b8ccce03d8dbd2d2
Andre Staltz <andre@staltz.com>
2020-01-31 13:45:27 +0200

    improve coverage of tests

--- test.js
+++ test.js
@@ -5,6 +5,7 @@ const test = require('tape');
 const pull = require('pull-stream');
 const ssbKeys = require('ssb-keys');
 const pullAsync = require('pull-async');
+const cat = require('pull-cat');
 
 const CreateTestSbot = require('ssb-server/index')
   .use(require('ssb-backlinks'))
@@ -55,6 +56,74 @@ test('threads.public gives a simple well-formed thread', t => {
 
       t.equals(msgs[2].value.content.root, rootKey, '3rd message is not root');
       t.equals(msgs[2].value.content.text, 'Third message');
+      myTestSbot.close();
+      t.end();
+    }),
+  );
+});
+
+test('threads.public can be called twice consecutively (to use cache)', t => {
+  const myTestSbot = CreateTestSbot({
+    path: fs.mkdtempSync(path.join(os.tmpdir(), 'conntest-')),
+    temp: true,
+    name: 'test1',
+    keys: lucyKeys,
+  });
+
+  const lucy = myTestSbot.createFeed(lucyKeys);
+
+  pull(
+    pullAsync(cb => {
+      lucy.add({ type: 'post', text: 'Thread root' }, cb);
+    }),
+    pull.asyncMap((rootMsg, cb) => {
+      lucy.add({ type: 'post', text: 'Second message', root: rootMsg.key }, cb);
+    }),
+    pull.asyncMap((prevMsg, cb) => {
+      const rootKey = prevMsg.value.content.root;
+      lucy.add({ type: 'post', text: 'Third message', root: rootKey }, cb);
+    }),
+    pull.map(() =>
+      cat([myTestSbot.threads.public({}), myTestSbot.threads.public({})]),
+    ),
+    pull.flatten(),
+
+    pull.collect((err, threads) => {
+      t.error(err);
+      t.equals(threads.length, 2, 'two threads');
+
+      const thread1 = threads[0];
+      const msgs1 = thread1.messages;
+      const root1 = msgs1[0].key;
+
+      t.equals(thread1.full, true, 'thread 1 comes back full');
+      t.equals(thread1.messages.length, 3, 'thread 1 has 3 messages');
+
+      t.equals(msgs1[0].value.content.root, undefined, '1st message is root');
+      t.equals(msgs1[0].value.content.text, 'Thread root');
+
+      t.equals(msgs1[1].value.content.root, root1, '2nd message is not root');
+      t.equals(msgs1[1].value.content.text, 'Second message');
+
+      t.equals(msgs1[2].value.content.root, root1, '3rd message is not root');
+      t.equals(msgs1[2].value.content.text, 'Third message');
+
+      const thread2 = threads[1];
+      const msgs2 = thread2.messages;
+      const root2 = msgs2[0].key;
+
+      t.equals(thread2.full, true, 'thread 2 comes back full');
+      t.equals(thread2.messages.length, 3, 'thread 2 has 3 messages');
+      t.equals(root2, root1, 'same root as before');
+
+      t.equals(msgs2[0].value.content.root, undefined, '1st message is root');
+      t.equals(msgs2[0].value.content.text, 'Thread root');
+
+      t.equals(msgs2[1].value.content.root, root2, '2nd message is not root');
+      t.equals(msgs2[1].value.content.text, 'Second message');
+
+      t.equals(msgs2[2].value.content.root, root2, '3rd message is not root');
+      t.equals(msgs2[2].value.content.text, 'Third message');
       myTestSbot.close();
       t.end();
     }),
@@ -401,6 +470,53 @@ test('threads.publicUpdates notifies of new thread or new msg', t => {
   );
 });
 
+test('threads.profile gives threads for lucy not mary', t => {
+  const myTestSbot = CreateTestSbot({
+    path: fs.mkdtempSync(path.join(os.tmpdir(), 'conntest-')),
+    temp: true,
+    name: 'test1',
+    keys: lucyKeys,
+  });
+
+  const lucy = myTestSbot.createFeed(lucyKeys);
+  const mary = myTestSbot.createFeed(maryKeys);
+  let rootId;
+
+  pull(
+    pullAsync(cb => {
+      lucy.add({ type: 'post', text: 'Root from lucy' }, cb);
+    }),
+    pull.asyncMap((rootMsg, cb) => {
+      rootId = rootMsg.key;
+      mary.add({ type: 'post', text: 'Root from mary' }, cb);
+    }),
+    pull.asyncMap((_, cb) => {
+      lucy.add({ type: 'post', text: 'Reply from lucy', root: rootId }, cb);
+    }),
+    pull.map(() => myTestSbot.threads.profile({ id: lucy.id })),
+    pull.flatten(),
+
+    pull.collect((err, threads) => {
+      t.error(err);
+      t.equals(threads.length, 1, 'only one thread');
+      const thread = threads[0];
+      t.equals(thread.full, true, 'thread comes back full');
+      t.equals(thread.messages.length, 2, 'thread has 2 messages');
+
+      const msgs = thread.messages;
+      const rootKey = msgs[0].key;
+      t.equals(msgs[0].value.content.root, undefined, '1st message is root');
+      t.equals(msgs[0].value.content.text, 'Root from lucy');
+
+      t.equals(msgs[1].value.content.root, rootKey, '2nd message is not root');
+      t.equals(msgs[1].value.content.text, 'Reply from lucy');
+
+      myTestSbot.close();
+      t.end();
+    }),
+  );
+});
+
 test('threads.private gives a simple well-formed thread', t => {
   const myTestSbot = CreateTestSbot({
     path: fs.mkdtempSync(path.join(os.tmpdir(), 'conntest-')),
@@ -505,6 +621,62 @@ test('threads.thread gives one full thread', t => {
       t.equals(thread.messages[0].value.content.text, 'A: root', 'root msg ok');
       t.equals(thread.messages[1].value.content.text, 'A: 2nd', '2nd msg ok');
       t.equals(thread.messages[2].value.content.text, 'A: 3rd', '3rd msg ok');
+      myTestSbot.close();
+      t.end();
+    }),
+  );
+});
+
+test('threads.thread can be called twice consecutively (to use cache)', t => {
+  const myTestSbot = CreateTestSbot({
+    path: fs.mkdtempSync(path.join(os.tmpdir(), 'conntest-')),
+    temp: true,
+    name: 'test7',
+    keys: lucyKeys,
+  });
+
+  const lucy = myTestSbot.createFeed(lucyKeys);
+
+  let rootAkey;
+  pull(
+    pullAsync(cb => {
+      lucy.add({ type: 'post', text: 'A: root' }, cb);
+    }),
+    pull.asyncMap((rootMsg, cb) => {
+      rootAkey = rootMsg.key;
+      lucy.add({ type: 'post', text: 'A: 2nd', root: rootMsg.key }, cb);
+    }),
+    pull.asyncMap((prevMsg, cb) => {
+      const rootKey = prevMsg.value.content.root;
+      lucy.add({ type: 'post', text: 'A: 3rd', root: rootKey }, cb);
+    }),
+
+    pull.map(() =>
+      cat([
+        myTestSbot.threads.thread({ root: rootAkey }),
+        myTestSbot.threads.thread({ root: rootAkey }),
+      ]),
+    ),
+    pull.flatten(),
+
+    pull.collect((err, threads) => {
+      t.error(err, 'no error');
+      t.equals(threads.length, 2, 'two threads');
+
+      const t1 = threads[0];
+      t.equals(t1.full, true, 'thread 1 comes back full');
+      t.equals(t1.messages.length, 3, 'thread 1 has 3 messages');
+      t.equals(t1.messages[0].value.content.text, 'A: root', 'root msg ok');
+      t.equals(t1.messages[1].value.content.text, 'A: 2nd', '2nd msg ok');
+      t.equals(t1.messages[2].value.content.text, 'A: 3rd', '3rd msg ok');
+
+      const t2 = threads[0];
+      t.equals(t2.full, true, 'thread 2 comes back full');
+      t.equals(t2.messages[0].key, t1.messages[0].key, 'same root as before');
+      t.equals(t2.messages.length, 3, 'thread 2 has 3 messages');
+      t.equals(t2.messages[0].value.content.text, 'A: root', 'root msg ok');
+      t.equals(t2.messages[1].value.content.text, 'A: 2nd', '2nd msg ok');
+      t.equals(t2.messages[2].value.content.text, 'A: 3rd', '3rd msg ok');
       myTestSbot.close();
       t.end();
     }),
